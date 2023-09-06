4db43777e846c2c0c7c65a732f84b0c629a81a2f
rinij <rinimani7@gmail.com>
2023-09-06 12:36:15 -0600

    solution

---
+++ sumOfEvenNumbers.js
@@ -0,0 +1,19 @@
+// Exercise: Sum of Even Numbers
+
+// Statement: Write an algorithm that calculates the sum of all even numbers between 1 and a given positive integer, inclusive.
+
+// Example:
+// If the user enters 10, the algorithm should calculate the sum of even numbers between 1 and 10, which are 2 + 4 + 6 + 8 + 10, resulting in an answer of 30.
+
+// Constraints:
+
+// The algorithm should work for any given positive integer.
+// Make sure to handle cases where the given number is odd or negative.
+
+// function sumOfEvenNumbers(number) {
+// let sum = 0;
+// for (let i = 1;i <= number ;i += 2 )
+// if( i%2 === 0)
+// sum = sum + i;
+// }
+// return(sum)
