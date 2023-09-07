c34929fafb6a30c52586e9b4c1d86ec7b3d35feb
rinij <rinimani7@gmail.com>
2023-09-07 14:45:58 -0600

    multilicationTable solution

---
+++ multiplicationTable.js
@@ -0,0 +1,8 @@
+function multiplicationTable(n) {
+  for (let i = 1; i <= n; i++) {
+    for (let j = 1; j <= 10; j++) {
+      console.log(`${i} * ${j} = ${i*j}`);
+    }
+  }
+}
+multiplicationTable(3);
