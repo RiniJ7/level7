b067f76b003375bab3d93d281e91e07232adeace
rinij <rinimani7@gmail.com>
2023-09-08 11:16:10 -0600

    reversing the elements in an array

---
+++ reverseArray.js
@@ -0,0 +1,14 @@
+// create a function that reverses an array
+
+function reverseArray(arrayInput) {
+  let arrayOutput = [];
+  for (let i = arrayInput.length - 1; i >= 0; i--) {
+    arrayOutput.push(arrayInput[i]);
+  }
+  console.log(arrayOutput);
+  return arrayOutput;
+}
+
+
+
+reverseArray([1, 2, 3, 4]);
